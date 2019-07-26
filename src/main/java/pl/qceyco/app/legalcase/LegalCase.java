d6ab9db7b678dd98870298ca3a3ed1a7e0e2ba4c
PawelQc <kucejko.pawel@gmail.com>
2019-07-26 17:46:47 +0200

    add Case validation

--- src/main/java/pl/qceyco/app/legalcase/LegalCase.java
+++ src/main/java/pl/qceyco/app/legalcase/LegalCase.java
@@ -1,30 +1,48 @@
 package pl.qceyco.app.legalcase;
 
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 import pl.qceyco.app.client.Client;
 import pl.qceyco.app.employee.Employee;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "legal_case")
+@Table(name = "cases")
 public class LegalCase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 10)
+    @Size(min = 4, max = 10)
+    @NotBlank
     private String signature;
 
+    @Column(length = 50)
+    @Size(min = 5, max = 50)
+    @NotBlank
     private String name;
 
+    @Column(length = 200)
+    @Size(min = 5, max = 200)
+    @NotBlank
     private String description;
 
     @ManyToMany
+    @JoinTable(name = "case_employee",
+            joinColumns = @JoinColumn(name = "case_id"),
+            inverseJoinColumns = @JoinColumn(name = "employee_id"))
+    @NotEmpty
     private List<Employee> projectTeam = new ArrayList<>();
 
     @ManyToOne
+    @NotNull
     private Client client;
 
     public Long getId() {
