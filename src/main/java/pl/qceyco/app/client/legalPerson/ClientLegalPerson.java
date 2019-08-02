b22acff43bdcc48f8dfda0cef5b497dd884f5001
PawelQc <kucejko.pawel@gmail.com>
2019-08-02 19:47:18 +0200

    test

--- src/main/java/pl/qceyco/app/client/legalPerson/ClientLegalPerson.java
+++ src/main/java/pl/qceyco/app/client/legalPerson/ClientLegalPerson.java
@@ -13,6 +13,7 @@ import javax.validation.constraints.Size;
 @Table(name = "clients_legal_person")
 public class ClientLegalPerson extends Client {
 
+
     @Column(name = "company_name", length = 100)
     @Size(min = 2, max = 100)
     @NotBlank
