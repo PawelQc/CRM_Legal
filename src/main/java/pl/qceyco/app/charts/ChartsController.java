bd0a0f366906c355d16ae1082130951d7b6d8a7f
PawelQc <kucejko.pawel@gmail.com>
2019-08-22 15:26:03 +0200

    test with charts

---
+++ src/main/java/pl/qceyco/app/charts/ChartsController.java
@@ -0,0 +1,80 @@
+package pl.qceyco.app.charts;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.qceyco.app.employee.EmployeeRepository;
+import pl.qceyco.app.project.Project;
+import pl.qceyco.app.project.ProjectRepository;
+import pl.qceyco.app.timesheet.referenceUnit.TimesheetReferenceUnit;
+import pl.qceyco.app.timesheet.referenceUnit.TimesheetReferenceUnitRepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.time.temporal.TemporalAdjusters.firstInMonth;
+
+@Controller
+public class ChartsController {
+
+    private final ProjectRepository projectRepository;
+    private final TimesheetReferenceUnitRepository timesheetReferenceUnitRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public ChartsController(ProjectRepository projectRepository, TimesheetReferenceUnitRepository timesheetReferenceUnitRepository, EmployeeRepository employeeRepository) {
+        this.projectRepository = projectRepository;
+        this.timesheetReferenceUnitRepository = timesheetReferenceUnitRepository;
+        this.employeeRepository = employeeRepository;
+    }
+
+
+    @RequestMapping(value = "/charts", method = RequestMethod.GET)
+    public String showStartPage() {
+
+        LocalDate thisMonthFirstMonday = LocalDate.now().with(firstInMonth(DayOfWeek.MONDAY));
+        LocalDate endDate = thisMonthFirstMonday.plusDays(27);
+        List<Project> projects = projectRepository.findAll();
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (Project p : projects) {
+            Integer hours = 0;
+            List<TimesheetReferenceUnit> projectTimesheets = timesheetReferenceUnitRepository.findAllByProjectIn4Weeks(p.getId(), thisMonthFirstMonday, endDate);
+            for (TimesheetReferenceUnit t : projectTimesheets) {
+                hours += t.countWeekHours();
+            }
+            dataset.setValue(hours, "Hours", p.getSignature());
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart("Work hours on projects",
+                "Projects", "Hours", dataset, PlotOrientation.VERTICAL,
+                false, true, false);
+        try {
+            ChartUtilities.saveChartAsJPEG(new File("/home/pawelqc/Pulpit/charts/chart.jpg"), chart, 1000, 600);
+        } catch (IOException e) {
+            System.err.println("Problem occurred creating chart.");
+        }
+
+
+        return "chart";
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ src/main/java/pl/qceyco/app/charts/ChartsController2.java
@@ -0,0 +1,28 @@
+package pl.qceyco.app.charts;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class ChartsController2 {
+
+
+
+
+}
+
+
+
+
+
+
+
