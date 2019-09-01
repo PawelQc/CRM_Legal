15b262c137055b0b3181ad6fbd99905643cb66c5
PawelQc <kucejko.pawel@gmail.com>
2019-09-01 18:09:07 +0200

    add minor changes

--- src/main/java/pl/qceyco/app/exception/DefaultExceptionHandler.java
+++ src/main/java/pl/qceyco/app/exception/DefaultExceptionHandler.java
@@ -4,8 +4,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLSyntaxErrorException;
+
 @ControllerAdvice
 public class DefaultExceptionHandler {
+
+    @ExceptionHandler(value = SQLSyntaxErrorException.class)
+    public String handleDBException(SQLSyntaxErrorException e) {
+        return "dataBaseError";
+    }
 
     @ExceptionHandler(value = Exception.class)
     public String handleDefaultException(Exception e, Model model) {
