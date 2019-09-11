8211e6c25e3d65b984994373b88d434f3c8b215a
PawelQc <kucejko.pawel@gmail.com>
2019-09-11 12:36:19 +0200

    see exception

--- src/main/java/pl/qceyco/app/exception/DefaultExceptionHandler.java
+++ src/main/java/pl/qceyco/app/exception/DefaultExceptionHandler.java
@@ -1,24 +1,24 @@
-package pl.qceyco.app.exception;
-
-import org.hibernate.exception.SQLGrammarException;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class DefaultExceptionHandler {
-
-//    @ExceptionHandler(value = SQLGrammarException.class)
-//    public String handleDBException(SQLGrammarException e) {
-//        return "dataBaseError";
+//package pl.qceyco.app.exception;
+//
+//import org.hibernate.exception.SQLGrammarException;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@ControllerAdvice
+//public class DefaultExceptionHandler {
+//
+////    @ExceptionHandler(value = SQLGrammarException.class)
+////    public String handleDBException(SQLGrammarException e) {
+////        return "dataBaseError";
+////    }
+//
+//    @ExceptionHandler(value = Exception.class)
+//    public String handleDefaultException(Exception e, Model model) {
+//        model.addAttribute("error", "Error: something went wrong...");
+//        model.addAttribute("errorMessage", "Cause: " + e.getMessage());
+//        return "error";
 //    }
-
-    @ExceptionHandler(value = Exception.class)
-    public String handleDefaultException(Exception e, Model model) {
-        model.addAttribute("error", "Error: something went wrong...");
-        model.addAttribute("errorMessage", "Cause: " + e.getMessage());
-        return "error";
-    }
-
-
-}
+//
+//
+//}
