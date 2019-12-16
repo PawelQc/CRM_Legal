f670c81c0260ae1b99054ddf87bc40102d87c330
PawelQc <kucejko.pawel@gmail.com>
2019-12-16 18:46:23 +0100

    add logger

--- src/main/java/pl/qceyco/exception/DefaultExceptionHandler.java
+++ src/main/java/pl/qceyco/exception/DefaultExceptionHandler.java
@@ -1,5 +1,7 @@
 package pl.qceyco.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class DefaultExceptionHandler {
 
+    private static final Logger LOGGER = LogManager.getLogger(DefaultExceptionHandler.class.getName());
+
     @ExceptionHandler(value = Exception.class)
     public String handleDefaultException(Exception e, Model model) {
         model.addAttribute("error", "Error: something went wrong...");
         model.addAttribute("errorMessage", "Cause: " + e.getMessage());
+        LOGGER.error(e.getMessage());
         return "error";
     }
 
